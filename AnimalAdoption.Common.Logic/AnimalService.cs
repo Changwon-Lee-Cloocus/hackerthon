0989d5ebca7ad2077d84fda46f658f850e0e8dac
bluecwon <bluecwon@cloocus.com>
2021-05-21 14:06:53 +0900

    update

--- AnimalAdoption.Common.Logic/AnimalService.cs
+++ AnimalAdoption.Common.Logic/AnimalService.cs
@@ -8,7 +8,7 @@ namespace AnimalAdoption.Common.Logic
     {
         public Animal[] ListAnimals => new Animal[] {
             new Animal { Id = 1, Name = "cwon", Age = 50, Description = "Soft natured" },
-            new Animal { Id = 2, Name = "Metamorph", Age = 50, Description = "Under a lot of pressure" },
+            new Animal { Id = 2, Name = "hello", Age = 50, Description = "Under a lot of pressure" },
             new Animal { Id = 3, Name = "Igno", Age = 50, Description = "Shiny and glasslike" },
         };
     }
